5fbe064bd9e6b48aee1b911448ad2036e373c18d
charles <rmswo417@naver.com>
2024-07-10 18:35:07 +0900

    업데이트 부분 완성

---
+++ lib/bucket_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+/// Bucket 담당
+class BucketService extends ChangeNotifier {
+  List<Bucket> bucketList = [
+    Bucket('잠자기', false), // 더미(dummy) 데이터
+  ];
+
+  /// bucket 추가
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+    notifyListeners();
+  }
+
+  /// bucket 수정
+  void updateBucket(Bucket bucket, int index) {
+    bucketList[index] = bucket;
+    notifyListeners();
+  }
+}
